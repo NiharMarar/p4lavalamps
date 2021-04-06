061f742929b56c769779f74274ca5646e6e053ae
NiharMarar <mararnihar@gmail.com>
2021-04-05 21:53:55 -0700

    minilab

--- blueprint/nihgarmarar/app.py
+++ blueprint/nihgarmarar/app.py
@@ -1,10 +1,39 @@
-from flask import Blueprint
+import os
+from flask import Flask, Blueprint, render_template, flash, redirect, url_for, session, logging
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+#from bs4 import BeautifulSoup
+import smtplib
+import time
+from booksearch.selection import Books
 
-y2021_tri1_bp = Blueprint('y2021_tri1', __name__,
+
+booksearch_bp = Blueprint('booksearch', __name__,
                           template_folder='templates',
                           static_folder='static', static_url_path='assets')
 
 
-@y2021_tri1_bp.route('/')
-def index():
-    return "Y2021 tri1 Home Site"
+@booksearch_bp.route('/', methods=['GET', 'POST'])
+def search():
+    #genres = 'nothing'
+    #if request.method == 'POST':
+    #genres = request.form.getlist('genre')
+    #print(genres)
+    #if 'Romance' and 'SciFi' and 'Nonfiction' and 'Comedy' in genres:
+    #    return render_template("allgenres.html")
+    #if 'Comedy' in genres:
+    #    return render_template("index.html")
+    #if 'Nonfiction' in genres:
+    #    return render_template("index.html")
+    #if 'Romance' in genres:
+    #    return render_template("index.html")
+    #if 'SciFi' in genres:
+    #    return render_template("index.html")
+    if request.method == 'POST':
+        return render_template("select-book.html", bookrecs=Books(int(request.form.get("series"))))
+    return render_template("select-book.html", bookrecs=Books(1))
+
+@booksearch_bp.route('/allgenres', methods=['GET', 'POST'])
+def allgenres():
+    return render_template("allgenres.html")
--- blueprint/nihgarmarar/minilab_NM.py
+++ blueprint/nihgarmarar/minilab_NM.py
@@ -1,34 +1,34 @@
+import random
 
-#1: setting up the class
+
+booklist1 = ["Fault in our Stars,", "Harry Potter Volume 1", "Percy Jackson", "Calculus 1 Textbook", "Scarlet Letter", "Romeo & Juliet", "Sesame Street", "Coding for Kids"]
+
+booklist2 = ["Fault in our Stars,", "Harry Potter Volume 1", "Romeo & Juliet"]
+
+
+
 class Books:
     """Initializer of class takes series parameter and returns Class Objects"""
-    def __init__(self, series):
+    def __init__(self, recs):
         """Built in validation and exception"""
-        if series < 0 or series > 6:
+        if recs < 0 or recs > 6:
             raise ValueError("Series must be between 0 and 6")
-        self._series = series
+        self._recs = recs
         self._list = []
         self._dict = {}
         self._dictID = 0
         # Duration timeElapsed;
         # Instant start = Instant.now();  // time capture -- start
-        self.book_series()
+        self.book_recs()
         # Instant end = Instant.now();    // time capture -- end
         # this.timeElapsed = Duration.between(start, end);
 
-
-    #2 defining your series
     """Algorithm for building book series list, this id called from __init__"""
-    def book_series(self):
-        limit = self._series
-        f = [(random.sample((booklist1), k=3))]
-        while limit > 0:
-            self.set_data(f[0])
-            f = [f[0]]
-            limit -= 1
+    def book_recs(self):
+        f = [(random.sample((booklist1), k=self._recs))]
+        self.set_data(f[0])
+        f = [f[0]]
 
-
-    #3: function to set data
     """Method/Function to set data: list, dict, and dictID are instance variables of Class"""
     def set_data(self, num):
         self._list.append(num)
@@ -36,7 +36,6 @@ class Books:
         self._dictID += 1
 
 
-    #4: creating our getters
     """Getters with decorator to allow . notation access"""
     @property
     def series(self):
@@ -56,60 +55,12 @@ class Books:
 
 
 
-#5: creating our objects and values + testing
 if __name__ == "__main__":
     '''Value for testing'''
-    a = 3
+
     '''Constructor of Class object'''
-    bookrecs = Books(a/a)
+    bookrecs = Books(3)
     print(f"Here are some book recomendations = {bookrecs.list}")
 
-
-
-
-
-#class FibonacciSeries:
-#def __init__(self, series):
-#if series < 2 or series > 100:
-#raise ValueError("Must be between 2 and 100")
-#self._series = series
-#self._list = []
-#self._dict = {}
-#self._dictID = 0
-#self.calculate_series()
-
-#def calculate_series(self):
-#limit = self._series
-#f = [0, 1]
-#while limit > 0:
-#self.set_data(f[0])
-#f = [f[1], f[0] + f[1]]
-#limit -= 1
-
-#def set_data(self, num):
-#self._list.append(num)
-#self._dict[self._dictID] = self._list.copy()
-#self._dictID += 1
-
-#@property
-#def series(self):
-#return self._series
-
-#@property
-#def list(self):
-#return self._list
-
-#@property
-#def number(self):
-#return self._list[self._dictID - 1]
-
-#def get_sequence(self, nth):
-#return self._dict[nth]
-
-#if __name__ == "__main__":
-#n = 69
-#fibonacci = FibonacciSeries(n)
-#print(f"Fibonacci number for {n} = {fibonacci.number}")
-#print(f"Fibonacci series for {n} = {fibonacci.list}")
-#for i in range(n):
-#print(f"Fibonacci sequence {i + 1} = {fibonacci.get_sequence(i)}")
+#for i in range(a):
+#print(f"Listing of Book Recs {i}= {bookrecs.get_sequence(i)}")
