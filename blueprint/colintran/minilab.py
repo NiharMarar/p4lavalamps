993edb1ca8a9f5f6cb34e0097228c27981445a93
colinktran <colint012204@gmail.com>
2021-04-30 23:07:44 -0700

    bubble sort minilab colin

--- blueprint/colintran/minilab.py
+++ blueprint/colintran/minilab.py
@@ -1,48 +1,26 @@
-# Python program for simple calculator
+class BubbleSort:
 
-# Function to add two numbers 
-def add(num1, num2):
-    return num1 + num2
+    def BubbleSort(self):
+        changes = passes = 0
+        last = len(self)
+        swapped = True
 
-# Function to subtract two numbers 
-def subtract(num1, num2):
-    return num1 - num2
+        while swapped:
+            swapped = False
+            passes += 1
+            for j in range(1, last):
+                if self[j - 1] > self[j]:
+                    self[j], self[j - 1] = self[j - 1], self[j]  # Swap
+                    changes += 1
+                    swapped = True
+                    last = j
 
-# Function to multiply two numbers
-def multiply(num1, num2):
-    return num1 * num2
+        print(self)
 
-# Function to divide two numbers
-def divide(num1, num2):
-    return num1 / num2
+    print("Bubble Sort")
 
-print("Please select operation -\n" \
-      "1. Add\n" \
-      "2. Subtract\n" \
-      "3. Multiply\n" \
-      "4. Divide\n")
-
-
-# Take input from the user
-select = int(input("Select operations form 1, 2, 3, 4 :"))
-
-number_1 = int(input("Enter first number: "))
-number_2 = int(input("Enter second number: "))
-
-if select == 1:
-    print(number_1, "+", number_2, "=",
-          add(number_1, number_2))
-
-elif select == 2:
-    print(number_1, "-", number_2, "=",
-          subtract(number_1, number_2))
-
-elif select == 3:
-    print(number_1, "*", number_2, "=",
-          multiply(number_1, number_2))
-
-elif select == 4:
-    print(number_1, "/", number_2, "=",
-          divide(number_1, number_2))
-else:
-    print("Invalid input")
+    while True:
+        print("Enter a list with spaces between each number.\nClick return when finished.")
+        numbers = input()
+        items = [int(num) for num in numbers.split() if num.isdigit()]
+        if items: BubbleSort()
--- main.py
+++ main.py
@@ -13,55 +13,67 @@ def home_route():
     else:
         return render_template("home.html")
 
-@app.route('/bfsort', methods = ['POST'])
+
+@app.route('/bfsort', methods=['POST'])
 def bfsort_bp():
     return render_template("bfsort.html")
 
 
-@app.route('/email', methods = ['POST'])
+@app.route('/email', methods=['POST'])
 def email():
     email = request.form['email']
-    email_text = 'Subject: {}\n\n{}'.format("United States Data", 'United States Total Cases 24,983,892; Total Deaths 2,080,972; Current Active Cases 25,361,201 ')
+    email_text = 'Subject: {}\n\n{}'.format("United States Data",
+                                            'United States Total Cases 24,983,892; Total Deaths 2,080,972; Current Active Cases 25,361,201 ')
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     server.login('wildcatsp4@gmail.com', 'MrMadman33')
     server.sendmail('wildcatsp4@gmail.com', email, email_text)
     server.close()
-    print ("email sent to:", email)
+    print("email sent to:", email)
     return render_template("home.html")
 
-@app.route('/bubblesort', methods = ['POST'])
+
+@app.route('/BubbleSort', methods=['POST'])
+def BubbleSort():
+    return render_template("testing.html")
+
+
+@app.route('/bubblesort', methods=['POST'])
 def bubblesort():
     return render_template("bubblesort.html")
 
-@app.route('/easteregg', methods = ['POST'])
+
+@app.route('/easteregg', methods=['POST'])
 def easteregg_bp():
     return render_template("easteregg.html")
 
-@app.route('/healthydinners', methods = ['POST'])
+
+@app.route('/healthydinners', methods=['POST'])
 def base():
     return render_template("healthydinners.html")
+
 
 @app.route('/healthylunches')
 def datatable():
     lunches = [
         {
-            "name":"Chicken Parmesian",
-            "price":10
+            "name": "Chicken Parmesian",
+            "price": 10
         },
 
         {
-            "name":"Chicken Stir Fry",
-            "price":15
+            "name": "Chicken Stir Fry",
+            "price": 15
         },
     ]
 
     if request.method == 'POST':
         lunches.append({
-            "name":request.form["name"],
-            "price":request.form["price"]
+            "name": request.form["name"],
+            "price": request.form["price"]
         })
     return render_template("healthylunches.html", lunches=lunches)
+
 
 @app.route('/main')
 def main():
