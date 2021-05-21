5a2468873ef9e65ccbc1dccaa2333409f76000fc
Bryce Modugno <bmodugno1000@gmail.com>
2021-05-20 23:15:00 -0700

    simple changes

--- bubblesort/bfbubble/app.py
+++ bubblesort/bfbubble/app.py
@@ -1,17 +1,17 @@
 from flask import Flask, Blueprint, render_template, request, redirect
-from bubblesort.Noahbubble.Noahbubble import BubbleSort
-from bubblesort.Noahbubble.Noahwords import alphabetize
-from bubblesort.Noahbubble.Noahnums import order
-bubblesort_Noahbubblesort_bp = Blueprint('Noahbubblesort', __name__,
+from bubblesort.bfbubble.bfbubble import BubbleSort
+from bubblesort.bfbubble.bfwords import alphabetize
+from bubblesort.bfbubble.bfnums import order
+bubblesort_bfbubblesort_bp = Blueprint('bfbubblesort', __name__,
                                          template_folder='templates',
                                          static_folder='static', static_url_path='assets')
 
-@bubblesort_Noahbubblesort_bp.route('/Noah_bubble', methods=["GET", "POST"])
-def Noahbubblesort():
+@bubblesort_bfbubblesort_bp.route('/bf_bubble', methods=["GET", "POST"])
+def bfbubblesort():
     if request.method == 'POST':
         form = request.form
         smallestNum = int(form["smallestNum"])
         largestNum = int(form["largestNum"])
         totalNum = int(form["totalNum"])
-        return render_template("Noahbubble.html", bubblesort=BubbleSort(smallestNum, largestNum, totalNum), word=alphabetize(form["words"]), num=order(form["nums"]))
-    return render_template("Noahbubble.html", bubblesort=BubbleSort(0, 10, 5))
+        return render_template("bfbubble.html", bubblesort=BubbleSort(smallestNum, largestNum, totalNum), word=alphabetize(form["words"]), num=order(form["nums"]))
+    return render_template("bfbubble.html", bubblesort=BubbleSort(0, 10, 5))
